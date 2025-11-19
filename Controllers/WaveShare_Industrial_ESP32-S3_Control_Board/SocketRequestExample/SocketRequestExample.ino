f3ac3056c273a7d99f866f18f4c1b22fc869b8b3
Frank Boddeke <frank@edgetech.eu>
2025-11-19 13:14:36 +0100

    diy controllers for charger
    diy controllers for charger

---
+++ Controllers/WaveShare_Industrial_ESP32-S3_Control_Board/SocketRequestExample/SocketRequestExample.ino
@@ -0,0 +1,58 @@
+#include <HardwareSerial.h>
+#include "Arduino.h"
+#include "ModBus.h"
+
+
+// ESP32 S3 Relay 1CH
+// product:    https://www.waveshare.com/esp32-s3-rs485-can.htm
+// schematics: https://files.waveshare.com/wiki/ESP32-S3-Relay-1CH/ESP32-S3-Relay-1CH-schematic.pdf
+// wiki:       https://www.waveshare.com/wiki/ESP32-S3-Relay-1CH-RS485
+
+
+// Board:
+// ESP32S3 Dev Module
+// Flash Size: 8MB (64Mb)
+// Partition Scheme: 8M with spiffs (3M APP/1.5MB SPIFFS)
+
+
+Modbus mb = Modbus();
+
+unsigned long next_time;
+
+void setup() {
+
+  printf("\r\n\r\nBoard: ESP32 S3 Relay 1CH\r\nTesting ModBus\r\n");
+
+  // modbus serial, mode and pins hard-coded
+  mb.begin(9600);
+
+  next_time = millis() + 1000;
+}
+
+void loop() {
+  
+  unsigned long now = millis();
+
+  if(now > next_time) {
+    
+    next_time += 5000;
+
+    printf("Request serial and fw version\r\n");
+
+    if (mb.readInputRegisters(1, 0, 6) == mb.ku8MBSuccess) {
+      
+      printf("serial: %4x%4x%4x%4x%4x, fw version: %4x\r\n",
+        mb.getResponseBuffer(0),
+        mb.getResponseBuffer(1),
+        mb.getResponseBuffer(2),
+        mb.getResponseBuffer(3),
+        mb.getResponseBuffer(4),
+        mb.getResponseBuffer(5));
+
+    } else {
+
+      printf("failed to read over mb");
+    }
+  }
+
+}
