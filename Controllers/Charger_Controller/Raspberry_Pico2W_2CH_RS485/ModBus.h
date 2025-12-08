017a7ca0fb2f6a5b5c1836c4cd60b4f8c9c02e60
Frank Boddeke <frank@edgetech.eu>
2025-12-08 23:31:56 +0100

    human readable states & errors
    human readable states & errors

--- Controllers/Charger_Controller/Raspberry_Pico2W_2CH_RS485/ModBus.h
+++ Controllers/Charger_Controller/Raspberry_Pico2W_2CH_RS485/ModBus.h
@@ -3,11 +3,9 @@
 
 #include "Arduino.h"
 
-
 #define lowWord(ww) ((uint16_t) ((ww) & 0xFFFF))
 #define highWord(ww) ((uint16_t) ((ww) >> 16))
 #define LONG(hi, lo) ((uint32_t) ((hi) << 16 | (lo)))
-
 
 class Modbus {
 public:
@@ -40,9 +38,6 @@ public:
 	void send(uint32_t);
 	uint8_t available(void);
 	uint16_t receive(void);
-
-	void enableTXpin(uint8_t);
-	void disableTXpin();
 	
 	uint8_t  readCoils(uint8_t, uint16_t, uint16_t);
 	uint8_t  readDiscreteInputs(uint8_t, uint16_t, uint16_t);
@@ -59,40 +54,32 @@ public:
 	uint8_t  readWriteMultipleRegisters(uint8_t, uint16_t, uint16_t);
 	
 private:
-	uint8_t  _u8SerialPort;									  ///< serial port (0..3) initialized in constructor
-	uint16_t _u16BaudRate;									   ///< baud rate (300..115200) initialized in begin()
-	static const uint8_t ku8MaxBufferSize				= 64;   ///< size of response/transmit buffers	
-	uint16_t _u16ReadAddress;									///< slave register from which to read
-	uint16_t _u16ReadQty;										///< quantity of words to read
-	uint16_t _u16ResponseBuffer[ku8MaxBufferSize];			   ///< buffer to store Modbus slave response; read via GetResponseBuffer()
-	uint16_t _u16WriteAddress;								   ///< slave register to which to write
-	uint16_t _u16WriteQty;									   ///< quantity of words to write
-	uint16_t _u16TransmitBuffer[ku8MaxBufferSize];			   ///< buffer containing data to transmit to Modbus slave; set via SetTransmitBuffer()
-	//uint16_t* txBuffer; // from Wire.h -- need to clean this up Rx
+	static const uint8_t ku8MaxBufferSize = 64;   	///< size of response/transmit buffers	
+	uint16_t _u16ReadAddress;												///< slave register from which to read
+	uint16_t _u16ReadQty;														///< quantity of words to read
+	uint16_t _u16ResponseBuffer[ku8MaxBufferSize];	///< buffer to store Modbus slave response; read via GetResponseBuffer()
+	uint16_t _u16WriteAddress;								   		///< slave register to which to write
+	uint16_t _u16WriteQty;									   			///< quantity of words to write
+	uint16_t _u16TransmitBuffer[ku8MaxBufferSize];	///< buffer containing data to transmit to Modbus slave; set via SetTransmitBuffer()
 	uint8_t _u8TransmitBufferIndex;
 	uint16_t u16TransmitBufferLength;
-	//uint16_t* rxBuffer; // from Wire.h -- need to clean this up Rx
 	uint8_t _u8ResponseBufferIndex;
 	uint8_t _u8ResponseBufferLength;
 	
-	// Modbus function codes for bit access
-	static const uint8_t ku8MBReadCoils				  = 0x01; ///< Modbus function 0x01 Read Coils
-	static const uint8_t ku8MBReadDiscreteInputs		 = 0x02; ///< Modbus function 0x02 Read Discrete Inputs
-	static const uint8_t ku8MBWriteSingleCoil			= 0x05; ///< Modbus function 0x05 Write Single Coil
-	static const uint8_t ku8MBWriteMultipleCoils		 = 0x0F; ///< Modbus function 0x0F Write Multiple Coils
+	static const uint8_t ku8MBReadCoils				  					= 0x01; ///< Modbus function 0x01 Read Coils
+	static const uint8_t ku8MBReadDiscreteInputs		 			= 0x02; ///< Modbus function 0x02 Read Discrete Inputs
+	static const uint8_t ku8MBWriteSingleCoil							= 0x05; ///< Modbus function 0x05 Write Single Coil
+	static const uint8_t ku8MBWriteMultipleCoils		 			= 0x0F; ///< Modbus function 0x0F Write Multiple Coils
 
-	// Modbus function codes for 16 bit access
-	static const uint8_t ku8MBReadHoldingRegisters	   = 0x03; ///< Modbus function 0x03 Read Holding Registers
-	static const uint8_t ku8MBReadInputRegisters		 = 0x04; ///< Modbus function 0x04 Read Input Registers
-	static const uint8_t ku8MBWriteSingleRegister		= 0x06; ///< Modbus function 0x06 Write Single Register
-	static const uint8_t ku8MBWriteMultipleRegisters	 = 0x10; ///< Modbus function 0x10 Write Multiple Registers
-	static const uint8_t ku8MBMaskWriteRegister		  = 0x16; ///< Modbus function 0x16 Mask Write Register
-	static const uint8_t ku8MBReadWriteMultipleRegisters = 0x17; ///< Modbus function 0x17 Read Write Multiple Registers
+	static const uint8_t ku8MBReadHoldingRegisters	  		= 0x03; ///< Modbus function 0x03 Read Holding Registers
+	static const uint8_t ku8MBReadInputRegisters		 			= 0x04; ///< Modbus function 0x04 Read Input Registers
+	static const uint8_t ku8MBWriteSingleRegister					= 0x06; ///< Modbus function 0x06 Write Single Register
+	static const uint8_t ku8MBWriteMultipleRegisters			= 0x10; ///< Modbus function 0x10 Write Multiple Registers
+	static const uint8_t ku8MBMaskWriteRegister		  			= 0x16; ///< Modbus function 0x16 Mask Write Register
+	static const uint8_t ku8MBReadWriteMultipleRegisters 	= 0x17; ///< Modbus function 0x17 Read Write Multiple Registers
 	
-	// Modbus timeout [milliseconds]
-	static const uint8_t ku8MBResponseTimeout			= 200;  ///< Modbus timeout [milliseconds]
+	static const uint8_t ku8MBResponseTimeout							=  200;  ///< Modbus timeout [milliseconds]
 	
-	// master function that conducts Modbus transactions
 	uint8_t ModbusMasterTransaction(uint8_t u8MBSlave, uint8_t u8MBFunction);
 };
 #endif
