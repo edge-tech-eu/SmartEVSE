05d3b06ef6370abc095420e785342f1b4bb3b4cc
Frank Boddeke <frank@edgetech.eu>
2025-12-08 23:06:11 +0100

    cleanup
    cleanup

--- Controllers/Charger_Controller/Raspberry_Pico2W_2CH_RS485/ModBus.h
+++ Controllers/Charger_Controller/Raspberry_Pico2W_2CH_RS485/ModBus.h
@@ -1,88 +1,14 @@
-/**
-@file
-Arduino library for communicating with Modbus slaves over RS232/485 (via RTU protocol).
-@defgroup setup ModbusMaster Object Instantiation/Initialization
-@defgroup buffer ModbusMaster Buffer Management
-@defgroup discrete Modbus Function Codes for Discrete Coils/Inputs
-@defgroup register Modbus Function Codes for Holding/Input Registers
-@defgroup constant Modbus Function Codes, Exception Codes
-*/
-/*
-  Modbus.h - Arduino library for communicating with Modbus slaves
-  over RS232/485 (via RTU protocol).
-  
-  This file is part of ModbusMaster.
-  
-  ModbusMaster is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-  
-  ModbusMaster is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-  
-  You should have received a copy of the GNU General Public License
-  along with ModbusMaster.  If not, see <http://www.gnu.org/licenses/>.
-  
-  Written by Doc Walker (Rx)
-  Copyright Â© 2009-2013 Doc Walker <4-20ma at wvfans dot net>
-  Adapted for Spark Core by Paul Kourany, March 14, 2014
-  Further modifications and port to Particle Photon by Anurag Chugh, July 5, 2016 <lithiumhead at gmail dot com>
-*/
-
-  
 #ifndef MODBUS_h
 #define MODBUS_h
 
-// adapted and hardcoded for WaveShare ESP32_S3_Relay_1CH
-
 #include "Arduino.h"
 
-/* _____UTILITY MACROS_______________________________________________________ */
-/**
-@def lowWord(ww) ((uint16_t) ((ww) & 0xFFFF))
-Macro to return low word of a 32-bit integer.
-*/
+
 #define lowWord(ww) ((uint16_t) ((ww) & 0xFFFF))
-
-
-/**
-@def highWord(ww) ((uint16_t) ((ww) >> 16))
-Macro to return high word of a 32-bit integer.
-*/
 #define highWord(ww) ((uint16_t) ((ww) >> 16))
-
-
-/**
-@def LONG(hi, lo) ((uint32_t) ((hi) << 16 | (lo)))
-Macro to generate 32-bit integer from (2) 16-bit words.
-*/
 #define LONG(hi, lo) ((uint32_t) ((hi) << 16 | (lo)))
 
 
-
-
-/* _____PROJECT INCLUDES_____________________________________________________ */
-// functions to calculate Modbus Application Data Unit CRC
-//#include <util/crc16.h>
-
-
-/*
-#define lowByte(w)					 ((w) & 0xFF)
-#define highByte(w)					(((w) >> 8) & 0xFF)
-#define bitRead(value, bit)			(((value) >> (bit)) & 0x01)
-#define bitSet(value, bit)			 ((value) |= (1UL << (bit)))
-#define bitClear(value, bit)		   ((value) &= ~(1UL << (bit)))
-#define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
-*/
-
-/* _____CLASS DEFINITIONS____________________________________________________ */
-/**
-Arduino class library for communicating with Modbus slaves over 
-RS232/485 (via RTU protocol).
-*/
 class Modbus {
 public:
 	Modbus();
@@ -91,117 +17,15 @@ public:
 	void begin();
 	void begin(uint16_t);
 	
-	// Modbus exception codes
-	/**
-	Modbus protocol illegal function exception.
-	
-	The function code received in the query is not an allowable action for
-	the server (or slave). This may be because the function code is only
-	applicable to newer devices, and was not implemented in the unit
-	selected. It could also indicate that the server (or slave) is in the
-	wrong state to process a request of this type, for example because it is
-	unconfigured and is being asked to return register values.
-	
-	@ingroup constant
-	*/
-	static const uint8_t ku8MBIllegalFunction			= 0x01;
-
-	/**
-	Modbus protocol illegal data address exception.
-	
-	The data address received in the query is not an allowable address for 
-	the server (or slave). More specifically, the combination of reference 
-	number and transfer length is invalid. For a controller with 100 
-	registers, the ADU addresses the first register as 0, and the last one 
-	as 99. If a request is submitted with a starting register address of 96 
-	and a quantity of registers of 4, then this request will successfully 
-	operate (address-wise at least) on registers 96, 97, 98, 99. If a 
-	request is submitted with a starting register address of 96 and a 
-	quantity of registers of 5, then this request will fail with Exception 
-	Code 0x02 "Illegal Data Address" since it attempts to operate on 
-	registers 96, 97, 98, 99 and 100, and there is no register with address 
-	100. 
-	
-	@ingroup constant
-	*/
-	static const uint8_t ku8MBIllegalDataAddress		 = 0x02;
-	
-	/**
-	Modbus protocol illegal data value exception.
-	
-	A value contained in the query data field is not an allowable value for 
-	server (or slave). This indicates a fault in the structure of the 
-	remainder of a complex request, such as that the implied length is 
-	incorrect. It specifically does NOT mean that a data item submitted for 
-	storage in a register has a value outside the expectation of the 
-	application program, since the MODBUS protocol is unaware of the 
-	significance of any particular value of any particular register.
-	
-	@ingroup constant
-	*/
-	static const uint8_t ku8MBIllegalDataValue		   = 0x03;
-	
-	/**
-	Modbus protocol slave device failure exception.
-	
-	An unrecoverable error occurred while the server (or slave) was
-	attempting to perform the requested action.
-	
-	@ingroup constant
-	*/
-	static const uint8_t ku8MBSlaveDeviceFailure		 = 0x04;
-
-	// Class-defined success/exception codes
-	/**
-	ModbusMaster success.
-	
-	Modbus transaction was successful; the following checks were valid:
-	  - slave ID
-	  - function code
-	  - response code
-	  - data
-	  - CRC
-	  
-	@ingroup constant
-	*/
-	static const uint8_t ku8MBSuccess					= 0x00;
-	
-	/**
-	ModbusMaster invalid response slave ID exception.
-	
-	The slave ID in the response does not match that of the request.
-	
-	@ingroup constant
-	*/
-	static const uint8_t ku8MBInvalidSlaveID			 = 0xE0;
-	
-	/**
-	ModbusMaster invalid response function exception.
-	
-	The function code in the response does not match that of the request.
-	
-	@ingroup constant
-	*/
-	static const uint8_t ku8MBInvalidFunction			= 0xE1;
-	
-	/**
-	ModbusMaster response timed out exception.
-	
-	The entire response was not received within the timeout period, 
-	ModbusMaster::ku8MBResponseTimeout. 
-	
-	@ingroup constant
-	*/
-	static const uint8_t ku8MBResponseTimedOut		   = 0xE2;
-	
-	/**
-	ModbusMaster invalid response CRC exception.
-	
-	The CRC in the response does not match the one calculated.
-	
-	@ingroup constant
-	*/
-	static const uint8_t ku8MBInvalidCRC				 = 0xE3;
+	static const uint8_t ku8MBSuccess									= 0x00;
+	static const uint8_t ku8MBIllegalFunction					= 0x01;
+	static const uint8_t ku8MBIllegalDataAddress		 	= 0x02;
+	static const uint8_t ku8MBIllegalDataValue		   	= 0x03;
+	static const uint8_t ku8MBSlaveDeviceFailure		 	= 0x04;
+	static const uint8_t ku8MBInvalidSlaveID			 		= 0xE0;
+	static const uint8_t ku8MBInvalidFunction					= 0xE1;
+	static const uint8_t ku8MBResponseTimedOut		   	= 0xE2;
+	static const uint8_t ku8MBInvalidCRC				 			= 0xE3;
 	
 	uint16_t getResponseBuffer(uint8_t);
 	void	 clearResponseBuffer();
@@ -216,9 +40,7 @@ public:
 	void send(uint32_t);
 	uint8_t available(void);
 	uint16_t receive(void);
-	
-	//void enableDebug();
-	//void disableDebug();
+
 	void enableTXpin(uint8_t);
 	void disableTXpin();
 	
